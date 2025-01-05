ecbd69f6afd0bd31b39aa8431d16039f80445efb
Emi <Emanuel.boehm@gmx.de>
2025-01-05 16:41:09 +0100

    2025

--- eww/scripts/active_window_title.sh
+++ eww/scripts/active_window_title.sh
@@ -1,3 +1,3 @@
 #!/bin/env bash
 hyprctl activewindow -j | jq --raw-output .class
-socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | stdbuf -o0 awk -F '>>|,' '{if(/activewindow>>/) print $2}' 
+socat -u UNIX-CONNECT:$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - | stdbuf -o0 awk -F '>>|,' '{if(/activewindow>>/) print $2}' 
--- fish/config.fish
+++ fish/config.fish
@@ -2,10 +2,21 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 set -U fish_user_paths $HOME/.cargo/bin $fish_user_paths
-# set -U fish_user_paths $HOME/.rustup $fish_user_paths
+set -U fish_user_paths $HOME/.local/bin $fish_user_paths
+set -U fish_user_paths $HOME/.local/share/gem/ruby/3.3.0/bin $fish_user_paths
 set -gx EDITOR helix
+bind \cz fg
 alias hx="helix"
+alias say="clear && cowsay -rC"
 
 function fish_greeting
     fortune
+end
+function yy
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
 end
